7905981b9e5d4bd5a3e851b1de27c1542fca6a76
Elena Lee <e.lee@visionlabs.ru>
2023-03-30 16:55:40 +0300

    add subtests for warped fisheye

--- tests/test_fisheye.py
+++ tests/test_fisheye.py
@@ -1,3 +1,7 @@
+from itertools import chain
+
+import pytest
+
 from lunavl.sdk.estimators.face_estimators.fisheye import Fisheye
 from lunavl.sdk.faceengine.setting_provider import DetectorType
 from lunavl.sdk.image_utils.image import VLImage
@@ -25,38 +29,53 @@ class TestFisheyeEffect(BaseTestClass):
         assert estimation.status
         assert 0 <= estimation.score <= 1
 
-    def estimate(self, image: str = FROWNING) -> Fisheye:
+    def estimate(self, image: str = FROWNING, warped: bool = True) -> Fisheye:
         """Estimate fisheye on image"""
         faceDetection = self.detector.detectOne(VLImage.load(filename=image))
-        warp = self.warper.warp(faceDetection)
-        estimation = self.fisheyeEstimator.estimate(warp.warpedImage)
+        if warped:
+            warp = self.warper.warp(faceDetection)
+            estimation = self.fisheyeEstimator.estimate(warp.warpedImage)
+        else:
+            estimation = self.fisheyeEstimator.estimate(faceDetection)
         assert isinstance(estimation, Fisheye)
         return estimation
 
-    def test_estimate_fisheye(self):
+    def estimateBatch(self, images, warped) -> list[Fisheye]:
+        """Estimate fisheye on image"""
+        imageDetections = self.detector.detect([VLImage.load(filename=name) for name in images])
+        if warped:
+            warps = [self.warper.warp(res[0]) for res in imageDetections]
+            estimations = self.fisheyeEstimator.estimateBatch(warps)
+        else:
+            estimations = self.fisheyeEstimator.estimateBatch(list(chain(*imageDetections)))
+        assert all(isinstance(estimation, Fisheye) for estimation in estimations)
+        return estimations
+
+    @pytest.mark.parametrize("warped", [True, False])
+    def test_estimate_fisheye(self, warped):
         """
         Simple fisheye estimation
         """
-        estimation = self.estimate(FROWNING)
+        estimation = self.estimate(FROWNING, warped=warped)
         assert not estimation.status
         assert 0 <= estimation.score <= 1
 
-    def test_fisheye_as_dict(self):
+    @pytest.mark.parametrize("warped", [True, False])
+    def test_fisheye_as_dict(self, warped):
         """
         Test method Fisheye.asDict
         """
-        estimation = self.estimate(FROWNING)
+        estimation = self.estimate(FROWNING, warped=warped)
         assert {
             "status": estimation.status,
             "score": estimation.score,
         } == estimation.asDict()
 
-    def test_estimate_fisheye_batch(self):
+    @pytest.mark.parametrize("warped", [True, False])
+    def test_estimate_fisheye_batch(self, warped):
         """
         Batch fisheye estimation test
         """
-        imageDetections = self.detector.detect([VLImage.load(filename=FROWNING), VLImage.load(filename=FISHEYE)])
-        warps = [self.warper.warp(res[0]) for res in imageDetections]
-        estimations = self.fisheyeEstimator.estimateBatch(warps)
+        estimations = self.estimateBatch([FROWNING, FISHEYE], warped=warped)
         assert not estimations[0].status
         assert estimations[1].status
