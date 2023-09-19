c84395bc784070f421866e3812d71ce663ac7040
Elena Lee <e.lee@visionlabs.ru>
2023-09-19 16:32:23 +0300

    support for sdk 5.16.0

--- tests/test_estimate_descriptor.py
+++ tests/test_estimate_descriptor.py
@@ -25,7 +25,7 @@ from tests.base import BaseTestClass
 from tests.detect_test_class import VLIMAGE_SMALL
 from tests.resources import BAD_THRESHOLD_WARP, HUMAN_WARP, WARP_CLEAN_FACE, WARP_WHITE_MAN
 
-EFDVa = EXISTENT_FACE_DESCRIPTOR_VERSION_ABUNDANCE = [59, 58, 60]
+EFDVa = EXISTENT_FACE_DESCRIPTOR_VERSION_ABUNDANCE = [59, 60, 62]
 
 EHDVa = EXISTENT_HUMAN_DESCRIPTOR_VERSION_ABUNDANCE = [107, 110, 108]
 
@@ -258,6 +258,7 @@ class TestEstimateDescriptor(BaseTestClass):
             58: 512,
             59: 512,
             60: 512,
+            62: 512,
             102: 2048,
             103: 2048,
             104: 2048,
@@ -446,7 +447,7 @@ class TestEstimateDescriptor(BaseTestClass):
                 _, descriptor = extractor.estimateDescriptorsBatch(
                     [faceWarp] * 2, aggregate=1, descriptorBatch=descriptorBatch
                 )
-                assert descriptor.garbageScore < 0.6, "Expected low gs"
+                assert descriptor.garbageScore < 0.93, "Expected low gs"
 
     def test_async_descriptor_estimation(self):
         """
