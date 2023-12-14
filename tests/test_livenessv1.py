1d85447f2006b0f19053c048d6483b460b8c57df
matemax <matemaks@ya.ru>
2023-12-14 13:01:42 +0300

    formats

--- tests/test_livenessv1.py
+++ tests/test_livenessv1.py
@@ -9,8 +9,14 @@ from lunavl.sdk.estimators.face_estimators.livenessv1 import LivenessPrediction,
 from lunavl.sdk.faceengine.setting_provider import DetectorType
 from lunavl.sdk.image_utils.image import VLImage
 from tests.base import BaseTestClass
-from tests.resources import CLEAN_ONE_FACE, FULL_FACE_WITH_MASK, LIVENESS_FACE, SPOOF, FULL_OCCLUDED_FACE, \
-    getPathToImage
+from tests.resources import (
+    CLEAN_ONE_FACE,
+    FULL_FACE_WITH_MASK,
+    FULL_OCCLUDED_FACE,
+    LIVENESS_FACE,
+    SPOOF,
+    getPathToImage,
+)
 from tests.schemas import LIVENESSV1_SCHEMA, jsonValidator
 
 
