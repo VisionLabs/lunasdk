fe99f2bfb7a3ff9eec9136461dfc800040e222d7
matemax <matemaks@ya.ru>
2024-10-28 17:34:19 +0300

    - UNKNOWN_LIVENESS

--- tests/test_livenessv1.py
+++ tests/test_livenessv1.py
@@ -9,10 +9,8 @@ from lunavl.sdk.estimators.face_estimators.livenessv1 import LivenessPrediction,
 from lunavl.sdk.faceengine.setting_provider import DetectorType
 from lunavl.sdk.image_utils.image import VLImage
 from tests.base import BaseTestClass
-from tests.resources import CLEAN_ONE_FACE, LIVENESS_FACE, SPOOF, UNKNOWN_LIVENESS
+from tests.resources import CLEAN_ONE_FACE, LIVENESS_FACE, SPOOF
 from tests.schemas import LIVENESSV1_SCHEMA, jsonValidator
-
-
 class TestEstimateLivenessV1(BaseTestClass):
     """
     Test liveness estimation.
@@ -65,7 +63,6 @@ class TestEstimateLivenessV1(BaseTestClass):
         cases = (
             Case(LIVENESS_FACE, LivenessPrediction.Real),
             Case(SPOOF, LivenessPrediction.Spoof),
-            Case(UNKNOWN_LIVENESS, LivenessPrediction.Unknown),
         )
         for case in cases:
             with self.subTest(prediction=case.prediction):
